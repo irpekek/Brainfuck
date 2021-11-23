8f1adf7cb5eeeb25e12e53760f0a4492f99b8c92
Sandy Laksana Putra <sandylaksanap@gmail.com>
2021-11-23 12:09:59 +0700

    Add Support Direct SSH (SSL/TLS) + Payload

--- .brainfuck-tunnel/app/tunnel.py
+++ .brainfuck-tunnel/app/tunnel.py
@@ -7,6 +7,7 @@ import socket
 import threading
 from .app import *
 
+
 class tunnel(threading.Thread):
     def __init__(self, socket_accept, tunnel_type):
         super(tunnel, self).__init__()
@@ -24,8 +25,10 @@ class tunnel(threading.Thread):
 
     def get_socket_client(self):
         socket_client, null = self.socket_accept
-        socket_client_request = socket_client.recv(self.buffer_size).decode('charmap')
-        self.host, self.port = re.findall(r'([^\s]+):([^\s]+)', socket_client_request)[0]
+        socket_client_request = socket_client.recv(
+            self.buffer_size).decode('charmap')
+        self.host, self.port = re.findall(
+            r'([^\s]+):([^\s]+)', socket_client_request)[0]
         self.host, self.port = str(self.host), int(self.port)
 
         return socket_client
@@ -43,7 +46,8 @@ class tunnel(threading.Thread):
         proxies = []
         for proxy in data_proxies:
             proxy = self.get_host_port(proxy)
-            if proxy: proxies.append(proxy)
+            if proxy:
+                proxies.append(proxy)
         proxy_host, proxy_port = random.choice(proxies)
 
         return str(proxy_host), int(proxy_port)
@@ -78,14 +82,15 @@ class tunnel(threading.Thread):
     def send_payload(self, payload_encode=''):
         payload_encode = payload_encode if payload_encode else '[method] [host_port] [protocol][crlf][crlf]'
         self.log('Payload: \n\n{}\n'.format(('|   ' + self.payload_decode(payload_encode).decode())
-            .replace('\r', '')
-            .replace('[split]', '$lf\n')
-            .replace('\n', '\n|   ')
-            .replace('$lf', '\n')
-        ))
+                                            .replace('\r', '')
+                                            .replace('[split]', '$lf\n')
+                                            .replace('\n', '\n|   ')
+                                            .replace('$lf', '\n')
+                                            ))
         payload_split = payload_encode.split('[split]')
         for i in range(len(payload_split)):
-            if i > 0: time.sleep(0.200)
+            if i > 0:
+                time.sleep(0.200)
             self.socket_tunnel.sendall(self.payload_decode(payload_split[i]))
 
     #
@@ -97,50 +102,63 @@ class tunnel(threading.Thread):
         if response.startswith('HTTP'):
             response = '\n\n|   {}\n'.format(response.replace('\n', '\n|   '))
         else:
-            response = '[W2]\n\n{}\n'.format(re.sub(r'\s+', ' ', response.replace('\n', '[CC][Y1]\\n[W2]')))
+            response = '[W2]\n\n{}\n'.format(
+                re.sub(r'\s+', ' ', response.replace('\n', '[CC][Y1]\\n[W2]')))
 
         return response
 
     def handler(self):
         sockets = [self.socket_tunnel, self.socket_client]
         timeout = 0
-        self.socket_client.sendall(b'HTTP/1.0 200 Connection established\r\n\r\n')
+        self.socket_client.sendall(
+            b'HTTP/1.0 200 Connection established\r\n\r\n')
         self.log('Connection established')
         while True:
             timeout += 1
             socket_io, null, errors = select.select(sockets, [], sockets, 3)
-            if errors: break
+            if errors:
+                break
             if socket_io:
                 for socket in socket_io:
                     try:
                         data = socket.recv(self.buffer_size)
-                        if not data: break
+                        if not data:
+                            break
                         if socket is self.socket_tunnel:
                             self.socket_client.sendall(data)
                         elif socket is self.socket_client:
                             self.socket_tunnel.sendall(data)
                         timeout = 0
-                    except: break
-            if timeout == 30: break
+                    except:
+                        break
+            if timeout == 30:
+                break
 
     def proxy_handler(self):
         x = 0
         while True:
-            if x == 1: self.log('Replacing response')
-            response = self.socket_tunnel.recv(self.buffer_size).decode('charmap')
-            if not response: break
+            if x == 1:
+                self.log('Replacing response')
+            response = self.socket_tunnel.recv(
+                self.buffer_size).decode('charmap')
+            if not response:
+                break
             response_status = response.replace('\r', '').split('\n')[0]
             if re.match(r'HTTP/\d(\.\d)? 200 .+', response_status):
-                self.log('Response: {}'.format(self.convert_response(response)))
+                self.log('Response: {}'.format(
+                    self.convert_response(response)))
                 self.handler()
                 break
             else:
-                self.log('Response: {}'.format(self.convert_response(response)))
-                self.socket_tunnel.sendall(b'HTTP/1.0 200 Connection established\r\nConnection: keep-alive\r\n\r\n')
+                self.log('Response: {}'.format(
+                    self.convert_response(response)))
+                self.socket_tunnel.sendall(
+                    b'HTTP/1.0 200 Connection established\r\nConnection: keep-alive\r\n\r\n')
                 x += 1
 
     def certificate(self):
-        self.log('Certificate:\n\n{}'.format(ssl.DER_cert_to_PEM_cert(self.socket_tunnel.getpeercert(True))))
+        self.log('Certificate:\n\n{}'.format(
+            ssl.DER_cert_to_PEM_cert(self.socket_tunnel.getpeercert(True))))
 
     #
     #
@@ -169,8 +187,10 @@ class tunnel(threading.Thread):
 
             self.log('Connecting to {} port {}'.format(self.host, self.port))
             self.socket_tunnel.connect((self.host, self.port))
-            self.log('Server name indication: {}'.format(self.server_name_indication))
-            self.socket_tunnel = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2).wrap_socket(self.socket_tunnel, server_hostname=self.server_name_indication, do_handshake_on_connect=self.do_handshake_on_connect)
+            self.log('Server name indication: {}'.format(
+                self.server_name_indication))
+            self.socket_tunnel = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2).wrap_socket(
+                self.socket_tunnel, server_hostname=self.server_name_indication, do_handshake_on_connect=self.do_handshake_on_connect)
             self.certificate()
             self.handler()
         except socket.timeout:
@@ -187,11 +207,34 @@ class tunnel(threading.Thread):
             self.proxy_host, self.proxy_port = self.get_proxy()
             self.payload = self.get_payload()
 
-            self.log('Connecting to remote proxy {} port {}'.format(self.proxy_host, self.proxy_port))
+            self.log('Connecting to remote proxy {} port {}'.format(
+                self.proxy_host, self.proxy_port))
             self.socket_tunnel.connect((self.proxy_host, self.proxy_port))
             self.log('Connecting to {} port {}'.format(self.host, self.port))
             self.send_payload(self.payload)
             self.proxy_handler()
+        except socket.timeout:
+            pass
+        except socket.error:
+            pass
+        finally:
+            self.socket_tunnel.close()
+            self.socket_client.close()
+
+    # Direct -> SSH (SSL/TLS) + Payload
+    def tunnel_type_3(self):
+        try:
+            self.server_name_indication = self.get_server_name_indication()
+            self.payload = self.get_payload()
+
+            self.log('Connecting to {} port {}'.format(self.host, self.port))
+            self.socket_tunnel.connect((self.host, self.port))
+            self.log('Server name indication: {}'.format(
+                self.server_name_indication))
+            self.socket_tunnel = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2).wrap_socket(
+                self.socket_tunnel, server_hostname=self.server_name_indication, do_handshake_on_connect=self.do_handshake_on_connect)
+            self.send_payload(self.payload)
+            self.handler()
         except socket.timeout:
             pass
         except socket.error:
@@ -207,6 +250,11 @@ class tunnel(threading.Thread):
 
         if not self.tunnel_type:
             pass
-        elif self.tunnel_type == '0': self.tunnel_type_0()
-        elif self.tunnel_type == '1': self.tunnel_type_1()
-        elif self.tunnel_type == '2': self.tunnel_type_2()
+        elif self.tunnel_type == '0':
+            self.tunnel_type_0()
+        elif self.tunnel_type == '1':
+            self.tunnel_type_1()
+        elif self.tunnel_type == '2':
+            self.tunnel_type_2()
+        elif self.tunnel_type == '3':
+            self.tunnel_type_3()
